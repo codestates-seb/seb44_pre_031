9a9b63d987bff839af660306c5fa871f07106fcb
jeongmin0218 <dlawjdals0218@gmail.com>
2023-06-16 08:40:15 +0900

    ♻ refactor: 공통 Entity 리팩터링 (2023.06.16 임정민)

--- server/src/main/java/com/codestates/pre_project/PreProjectApplication.java
+++ server/src/main/java/com/codestates/pre_project/PreProjectApplication.java
@@ -2,12 +2,12 @@ package com.codestates.pre_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PreProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PreProjectApplication.class, args);
 	}
-
 }
--- server/src/main/java/com/codestates/pre_project/module/vote/repository/VoteRepository.java
+++ server/src/main/java/com/codestates/pre_project/module/vote/repository/VoteRepository.java
@@ -5,8 +5,6 @@ import com.codestates.pre_project.module.question.entity.Question;
 import com.codestates.pre_project.module.vote.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface VoteRepository extends JpaRepository<Vote, Long> {
     Vote findByQuestionAndMember(Question question, Member member);
 }
