0f3bb871630b6d3ed3b6051ecb9bb6063ee7a993
yeonwoo <ppupy1209@naver.com>
2023-06-16 13:07:59 +0900

    :recycle: refactor: SecurityConfiguration 수정 (2023.06.16 김연우)

--- server/src/main/java/com/codestates/pre_project/global/config/SecurityConfiguration.java
+++ server/src/main/java/com/codestates/pre_project/global/config/SecurityConfiguration.java
@@ -1,15 +1,18 @@
-package com.codestates.pre_project.config;
+package com.codestates.pre_project.global.config;
 
 import com.codestates.pre_project.global.auth.filter.JwtAuthenticationFilter;
+import com.codestates.pre_project.global.auth.filter.JwtVerificationFilter;
 import com.codestates.pre_project.global.auth.handler.MemberAuthenticationFailureHandler;
 import com.codestates.pre_project.global.auth.handler.MemberAuthenticationSuccessHandler;
 import com.codestates.pre_project.global.auth.jwt.JwtTokenizer;
+import com.codestates.pre_project.global.auth.utils.CustomAuthorityUtils;
 import com.codestates.pre_project.member.repository.MemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,10 +26,12 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfiguration {
     private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
 
 
-    public SecurityConfiguration(JwtTokenizer jwtTokenizer) {
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils) {
         this.jwtTokenizer = jwtTokenizer;
+        this.authorityUtils = authorityUtils;
     }
 
     @Bean
@@ -36,6 +41,8 @@ public class SecurityConfiguration {
                   .and()
                   .csrf().disable()  // 로컬 환경에서 테스트하기 위함
                   .cors(withDefaults()) // CORS 설정 추가, 프론트엔드와 통신
+                  .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 생성하지 않음
+                  .and()
                   .formLogin().disable() // SSR 방식이 아닌 CSR 방식
                   .httpBasic().disable()
                   .apply(new CustomFilterConfigurer()) // 구현한 filter 등록 역할
@@ -55,7 +62,12 @@ public class SecurityConfiguration {
             jwtAuthenticationFilter.setFilterProcessesUrl("/api/users/sign-in"); // request URL 등록
             jwtAuthenticationFilter.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler()); // 인증 성공시
             jwtAuthenticationFilter.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler()); // 인증 실패시
-            builder.addFilter(jwtAuthenticationFilter);
+
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer,authorityUtils); // Jwt 검증 필터
+            builder
+                    .addFilter(jwtAuthenticationFilter)
+                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
+
         }
     }
 
