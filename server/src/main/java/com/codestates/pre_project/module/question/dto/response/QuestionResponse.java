6bb90928de9f5530a4219578b1c6b2445dfbf214
[Hayan] <[c_white723@naver.com]>
2023-06-16 14:37:52 +0900

    � chore: build.gradle querydsl 설정 추가 (2023.06.16 조하얀)

--- server/src/main/java/com/codestates/pre_project/module/question/dto/response/QuestionResponse.java
+++ server/src/main/java/com/codestates/pre_project/module/question/dto/response/QuestionResponse.java
@@ -1,9 +1,15 @@
 package com.codestates.pre_project.module.question.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class QuestionResponse {
     private String title;
     private String content;
     private Long voteCount;
     private boolean selectedAnswer;
-
 }
