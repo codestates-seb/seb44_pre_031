a9b9a1ba3896750a10b62fb69ebc879dd65c5107
[Hayan] <[c_white723@naver.com]>
2023-06-20 11:21:17 +0900

    ♻ refactor: 공통 exception 사용하도록 수정 (2023.06.20 조하얀)

--- server/src/main/java/com/codestates/pre_project/global/auth/userdetails/MemberDetailsService.java
+++ server/src/main/java/com/codestates/pre_project/global/auth/userdetails/MemberDetailsService.java
@@ -1,8 +1,8 @@
 package com.codestates.pre_project.global.auth.userdetails;
 
 import com.codestates.pre_project.global.auth.utils.CustomAuthorityUtils;
+import com.codestates.pre_project.global.exception.CustomException;
 import com.codestates.pre_project.module.member.entity.Member;
-import com.codestates.pre_project.module.member.exception.MemberNotFoundException;
 import com.codestates.pre_project.module.member.repository.MemberRepository;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,7 +11,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
-import java.util.Optional;
+
+import static com.codestates.pre_project.global.exception.ErrorCode.MEMBER_NOT_FOUND;
 
 @Component
 public class MemberDetailsService implements UserDetailsService {
@@ -27,10 +28,10 @@ public class MemberDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Member> optionalMember = memberRepository.findByEmail(username);
-        Member findMember = optionalMember.orElseThrow(() -> new MemberNotFoundException());
-
-
+        Member findMember = memberRepository.findByEmail(username)
+                .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+//        Optional<Member> optionalMember = memberRepository.findByEmail(username);
+//        Member findMember = optionalMember.orElseThrow(() -> new MemberNotFoundException());
         return new MemberDetails(findMember);
     }
 
