94230d13637efdfd832379b2369b3655239b7016
jihoon2819 <parkjihoon98>
2023-06-22 16:03:33 +0900

    2차병합

--- client/src/components/actionS.js
+++ client/src/components/actionS.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 
 // eslint-disable-next-line no-unused-vars
 export const actionS = createAsyncThunk('user/join', async (data, thunkAPI) => {
-  const result = await axios.post('http://localhost:8080/api/users/sign-up', {
-    displayName: data.displayName,
-    email: data.email,
-    password: data.password,
-  });
-
-  console.log(result);
+  const result = await axios.post(
+    'http://ec2-52-79-240-48.ap-northeast-2.compute.amazonaws.com:8080/api/users/sign-up',
+    {
+      displayName: data.displayName,
+      email: data.email,
+      password: data.password,
+    }
+  );
   return result;
 });
