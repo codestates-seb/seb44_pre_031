979c05687b978fb0aa5cff4da4fc96d71a1b0862
[Hayan] <[c_white723@naver.com]>
2023-06-22 11:23:10 +0900

    수정

--- server/scripts/server_start.sh
+++ server/scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/build
-sudo nohup java -jar DeployServer-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+sudo nohup java -jar pre_project-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
