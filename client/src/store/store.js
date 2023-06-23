5e467c4006a8b7f7eb17e4666aa83e47078c2a79
Jeon_Ye_Hun <yahun722@naver.com>
2023-06-23 19:19:03 +0900

    ✨feat: 페이지네이션 left,rigth 구현, slice 구현 (2023.06.23 전예훈)

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,12 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from '../slices/loginSlice';
 import signupReducer from '../slices/signupSlice';
 import questionReducer from '../slices/questionSlice';
-
+import paginationReducer from '../slices/paginationSlice';
 const store = configureStore({
   reducer: {
     question: questionReducer,
     login: loginReducer,
     signup: signupReducer,
+    pages: paginationReducer,
   },
 });
 
