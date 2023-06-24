35e48b107b94775b91c496ec9ee12355a5173c19
jeongmin0218 <dlawjdals0218@gmail.com>
2023-06-24 17:35:16 +0900

    ♻ refactor: 멤버 조회 리팩터링 (2023.06.24 임정민)

--- server/src/main/java/com/codestates/pre_project/module/member/dto/MemberDto.java
+++ server/src/main/java/com/codestates/pre_project/module/member/dto/MemberDto.java
@@ -4,14 +4,34 @@ import com.codestates.pre_project.module.member.entity.Member;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberDto {
-    private Long id;
+    @Length(min = 3, max = 15, message = "이름은 3에서 15자 이내")
     private String displayName;
+    private String location;
+    private String title;
+    private String aboutMe;
+    private String webLink;
+    private String twitterLink;
+    private String githubLink;
+    private String fullName;
 
     public static MemberDto of(Member member) {
-        return new MemberDto(member.getId(), member.getDisplayName());
+        return new MemberDto(
+                member.getDisplayName(),
+                member.getLocation(),
+                member.getTitle(),
+                member.getAboutMe(),
+                member.getWebLink(),
+                member.getTwitterLink(),
+                member.getGithubLink(),
+                member.getFullName()
+        );
     }
 }
